7248c28e904645de62e7397780fe2b1a4272aee1
Miguel Angel Dulcey Diaz <mpgigat@gmail.com>
2020-12-02 18:55:20 -0500

    Proyecto backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 
 const routes = require('./routes/estudiantes');
 
-const app = express();
+const app = express();            
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
