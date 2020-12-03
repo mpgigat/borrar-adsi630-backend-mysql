0a5d3152bae3ecde7f5ba499c5ea04f305b13b7b
Miguel Angel Dulcey Diaz <mpgigat@gmail.com>
2020-12-02 19:27:55 -0500

    Agregamos archivo readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## BACKEND MYSQL - NODE - ESTUDIANTES
+
+Aqui creamos un backend con node y accesamos a una base de datos mysql.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const routes = require('./routes/estudiantes');
-
+ 
 const app = express();            
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
